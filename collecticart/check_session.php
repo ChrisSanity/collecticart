36e44b046df6df64d9c2c36a00a06ee1b9f19114
ChrisSanity <batoctoy.287973@stamaria.sti.edu.ph>
2025-10-26 22:26:34 +0800

    Remove all files from the repository

--- collecticart/check_session.php
+++
@@ -1,8 +0,0 @@
-<?php
-require __DIR__ . "/session-config.php";
-
-if (isset($_SESSION['user_id'])) {
-    echo "LOGGED_IN";
-} else {
-    echo "NOT_LOGGED_IN";
-}
--- collecticart/crud-delete_product.php
+++
@@ -1,8 +0,0 @@
-<?php
-$conn = new mysqli("localhost", "root", "", "collecticart");
-
-$id = $_GET['id'];
-$conn->query("DELETE FROM products WHERE id=$id");
-header("Location: home.php");
-exit;
-?>
